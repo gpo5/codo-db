d1cc13c8ab0ee55a9e5359b9a39ae91d8ed76292
gpo5 <xm3n6x@gmail.com>
2024-07-02 13:07:31 -0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,17 +1,8 @@
 import mysql.connector
 
 database = mysql.connector.connect(
-    host='bsuysnqfzvkfiv0ailun-mysql.services.clever-cloud.com',
-    user='uovvvhdwipnqbzor',
-    password='Nw9eWMpGgdYSKM41Asj4',
-    database='bsuysnqfzvkfiv0ailun'
+    host='jdinucci.mysql.pythonanywhere-services.com',
+    user='jdinucci',
+    password='XXXXXXX',
+    database='jdinucci$cododb'
 )
-
-'''
-database = mysql.connector.connect(
-    host='localhost',
-    user='gpo5',
-    password='#Puente1915',
-    database='codo_db01'
-)
-'''
